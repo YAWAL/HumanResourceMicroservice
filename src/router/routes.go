986a17e32949e46ba48ee3ef64907b5cbdb3f2b0
ValeriiYavorskyi <valerii.yavorskyi@continuum.net>
2019-01-19 18:13:48 +0200

    add router

---
+++ src/router/routes.go
@@ -0,0 +1,34 @@
+package router
+
+import (
+	"net/http/pprof"
+
+	"github.com/YAWAL/ERP/back-end/HumanResourcesService/HRusecases"
+	"github.com/gorilla/mux"
+)
+
+var Router *mux.Router
+
+func init() {
+	Router = mux.NewRouter()
+
+	hrApi := Router.PathPrefix("/HRMS").Subrouter()
+	hrApi.HandleFunc("/", HRusecases.TempIndexPage)
+	hrApi.HandleFunc("/employees", HRusecases.ShowAllEmployees).Methods("GET")
+	hrApi.HandleFunc("/employees/{name:[a-z]+}", HRusecases.ShowAllEmployeesByName).Methods("GET")
+
+	// profiling
+	prof := Router.PathPrefix("/debug/pprof").Subrouter()
+	prof.HandleFunc("/", pprof.Index)
+	prof.HandleFunc("/cmdline", pprof.Cmdline)
+	prof.HandleFunc("/symbol", pprof.Symbol)
+	prof.HandleFunc("/trace", pprof.Trace)
+	profile := Router.PathPrefix("/profile").Subrouter()
+	profile.HandleFunc("", pprof.Profile)
+	profile.Handle("/goroutine", pprof.Handler("goroutine"))
+	profile.Handle("/threadcreate", pprof.Handler("threadcreate"))
+	profile.Handle("/heap", pprof.Handler("heap"))
+	profile.Handle("/block", pprof.Handler("block"))
+	profile.Handle("/mutex", pprof.Handler("mutex"))
+
+}

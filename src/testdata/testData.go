0538210752313e0306614d2aff803d8dce97ccff
ValeriiYavorskyi <valerii.yavorskyi@continuum.net>
2019-02-24 19:53:12 +0200

    update skeleton for test data

--- src/testdata/testData.go
+++ src/testdata/testData.go
@@ -1,63 +1,57 @@
 package testdata
 
 import (
-	"github.com/YAWAL/ERP-common-lib/models"
+	"github.com/YAWAL/HumanResourceMicroservice/src/HRrepository"
 	"time"
 )
 
-var Employees = []models.Employee{
-	models.Employee{
-		EmployeeId:        1,
+var Employees = []HRrepository.Employee{
+	{
+		EmployeeID:        1,
 		Name:              "petro",
 		LastName:          "1stLn",
 		MiddleName:        "",
 		PassSeriesNum:     "",
 		IdentificationNum: "",
-		AddressId:         1,
 		BirthDate:         time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
-		EmployeeType:      "",
 		Position:          "",
 		JoinDate:          time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
 		QuitDate:          time.Now(),
 		IsQuit:            false},
-	models.Employee{
-		EmployeeId:        2,
+	{
+		EmployeeID:        2,
 		Name:              "ivan",
 		LastName:          "2stLn",
 		MiddleName:        "",
 		PassSeriesNum:     "",
 		IdentificationNum: "",
-		AddressId:         2,
 		BirthDate:         time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
-		EmployeeType:      "",
 		Position:          "",
 		JoinDate:          time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
 		QuitDate:          time.Now(),
 		IsQuit:            false},
-	models.Employee{
-		EmployeeId:        3,
+	{
+		EmployeeID:        3,
 		Name:              "3st",
 		LastName:          "3stLn",
 		MiddleName:        "",
 		PassSeriesNum:     "",
 		IdentificationNum: "",
-		AddressId:         3, BirthDate: time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
-		EmployeeType: "",
-		Position:     "",
-		JoinDate:     time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
-		QuitDate:     time.Now(),
-		IsQuit:       false},
-	models.Employee{
-		EmployeeId:        4,
+		BirthDate:         time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
+		Position:          "",
+		JoinDate:          time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
+		QuitDate:          time.Now(),
+		IsQuit:            false},
+	{
+		EmployeeID:        4,
 		Name:              "ivan",
 		LastName:          "3stLn",
 		MiddleName:        "",
 		PassSeriesNum:     "",
 		IdentificationNum: "",
-		AddressId:         4, BirthDate: time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
-		EmployeeType: "",
-		Position:     "",
-		JoinDate:     time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
-		QuitDate:     time.Now(),
-		IsQuit:       false},
+		BirthDate:         time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
+		Position:          "",
+		JoinDate:          time.Date(1989, time.April, 0, 0, 0, 0, 0, time.Local),
+		QuitDate:          time.Now(),
+		IsQuit:            false},
 }

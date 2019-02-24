27d57de50a779431775b75546257bf7a7b013cc3
ValeriiYavorskyi <valerii.yavorskyi@continuum.net>
2019-02-24 19:53:56 +0200

    add address repository

---
+++ src/database/AddressRepository.go
@@ -0,0 +1,9 @@
+package database
+
+import "github.com/YAWAL/ERP-common-lib/models"
+
+type EmployeeAddress models.EmployeeAddress
+
+type AddressRepository interface {
+	GetEmployeeAddress(employeeID int64) *EmployeeAddress
+}
--- src/main.go
+++ src/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/YAWAL/HumanResourceMicroservice/src/logging"
 	"github.com/YAWAL/HumanResourceMicroservice/src/router"
 )
 
 func main() {
 
-	r := router.Router
+	r := router.InitRouter()
 
 	srv := &http.Server{
 		Handler: r,
 		Addr:    "127.0.0.1:8000",
 	}
-	logging.Log.Println(srv.ListenAndServe())
+	fmt.Println(srv.ListenAndServe())
 
 }

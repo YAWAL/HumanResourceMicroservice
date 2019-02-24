837d40cd5d81f18d2cc9bfb78468c2517ef543f0
ValeriiYavorskyi <valerii.yavorskyi@continuum.net>
2019-02-24 19:45:42 +0200

    update routers

--- src/router/routes.go
+++ src/router/routes.go
@@ -1,35 +1,21 @@
 package router
 
 import (
-	"github.com/YAWAL/HumanResourceMicroservice/src/HRusecases"
-	"net/http/pprof"
+	"github.com/YAWAL/HumanResourceMicroservice/src/handlers"
 
 	"github.com/gorilla/mux"
 )
 
-var Router *mux.Router
+func InitRouter() *mux.Router {
+	var router *mux.Router
 
-func init() {
-	Router = mux.NewRouter()
+	router = mux.NewRouter()
 
-	hrApi := Router.PathPrefix("/HRMS").Subrouter()
+	hrApi := router.PathPrefix("/HRMS").Subrouter()
 
-	hrApi.HandleFunc("/", HRusecases.TempIndexPage)
-	hrApi.HandleFunc("/employees", HRusecases.ShowAllEmployees).Methods("GET")
-	hrApi.HandleFunc("/employees/{name:[a-z]+}", HRusecases.ShowAllEmployeesByName).Methods("GET")
+	hrApi.HandleFunc("/", handlers.TempIndexPage)
+	hrApi.HandleFunc("/employees", handlers.ShowAllEmployees).Methods("GET")
+	hrApi.HandleFunc("/employees/{name:[a-z]+}", handlers.ShowAllEmployeesByName).Methods("GET")
 
-	// profiling
-	prof := Router.PathPrefix("/debug/pprof").Subrouter()
-	prof.HandleFunc("/", pprof.Index)
-	prof.HandleFunc("/cmdline", pprof.Cmdline)
-	prof.HandleFunc("/symbol", pprof.Symbol)
-	prof.HandleFunc("/trace", pprof.Trace)
-	profile := Router.PathPrefix("/profile").Subrouter()
-	profile.HandleFunc("", pprof.Profile)
-	profile.Handle("/goroutine", pprof.Handler("goroutine"))
-	profile.Handle("/threadcreate", pprof.Handler("threadcreate"))
-	profile.Handle("/heap", pprof.Handler("heap"))
-	profile.Handle("/block", pprof.Handler("block"))
-	profile.Handle("/mutex", pprof.Handler("mutex"))
-
+	return router
 }

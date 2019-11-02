2353b6c711bc80cd63150106c370d1586b407cee
YAWAL <yavorskyyval@gmail.com>
2019-11-02 16:23:45 +0200

    -

--- src/vendor/github.com/YAWAL/ERP-common-lib/models/department.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// Department represents data about departments stored in database
-type Department struct {
-	DepartmentID   int
-	DepartmentName string
-	ChairmanID     int64
-}
--- src/vendor/github.com/YAWAL/ERP-common-lib/models/workshop.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// Workshop represents data about workshops stored in database
-type Workshop struct {
-	WorkshopID   int
-	WorkshopName string
-	ChairmanID   int64
-}

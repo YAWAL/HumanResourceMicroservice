883e8aedfb2a1d00078cfc43e722eb07d82120fa
YAWAL <yavorskyyval@gmail.com>
2019-11-04 22:33:01 +0200

    updated database configuration struct acc to MongoDB needs

--- src/database/config.go
+++ src/database/config.go
@@ -1,9 +1,7 @@
 package database
 
-type Config struct {
-	Dialect      string `json:"dialect"`
-	User         string `json:"user"`
-	DataBaseName string `json:"db_name"`
-	SSLMode      string `json:"ssl_mode"`
-	Password     string `json:"password"`
+// MongoConfig is a structure which conteins details for creating MongoDB client
+type MongoConfig struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
 }

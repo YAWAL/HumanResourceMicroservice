2555b8d4ae98a0b2bc4764bf3dd5f717253b68f4
YAWAL <yavorskyyval@gmail.com>
2019-11-04 23:01:50 +0200

    removed old unit test

--- src/handlers/usecases_test.go
+++ src/handlers/usecases_test.go
@@ -1,15 +1,1 @@
 package handlers
-
-import (
-	"net/http"
-	"testing"
-)
-
-func TestShowAllEmployees(t *testing.T) {
-	var (
-		w http.ResponseWriter
-		r *http.Request
-	)
-
-	ShowAllEmployees(w, r)
-}

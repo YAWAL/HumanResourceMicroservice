a26998b3634563b093da7d37fa328307ad2341ff
YAWAL <yavorskyyval@gmail.com>
2019-11-08 01:04:31 +0200

    updated router with new paths and handler functions

--- src/router/routes.go
+++ src/router/routes.go
@@ -9,12 +9,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// InitRouter creates gorilla mux router and mapped paths with handler functions
 func InitRouter(er database.EmployeeRepository) (r *mux.Router) {
 	r = mux.NewRouter()
 	api := r.PathPrefix("/humanResources").Subrouter()
 	api.HandleFunc("/", handlers.TempIndexPage)
 	api.HandleFunc("/employees", handlers.ShowAllEmployees(er)).Methods(http.MethodGet)
 	api.HandleFunc("/employees", handlers.CreateEmployee(er)).Methods(http.MethodPost)
-	api.HandleFunc("/employees/{name:[a-z]+}", handlers.ShowAllEmployeesByName).Methods(http.MethodGet)
+	api.HandleFunc("/employees", handlers.UpdateEmployee(er)).Methods(http.MethodPut)
+	api.HandleFunc("/employees/{id:[0-9a-zA-Z]+}", handlers.DeleteEmployee(er)).Methods(http.MethodDelete)
+	api.HandleFunc("/employees/{position:[0-9a-zA-Z]+}", handlers.ShowAllEmployeesByPosition(er)).Methods(http.MethodGet)
 	return r
 }

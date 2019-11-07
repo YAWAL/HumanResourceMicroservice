88bbde463f0516299782c11eb6207b3ac7e4435f
YAWAL <yavorskyyval@gmail.com>
2019-11-08 01:05:39 +0200

    added new methods to Employee repository

--- src/database/EmployeeRepository.go
+++ src/database/EmployeeRepository.go
@@ -4,18 +4,19 @@ import (
 	"github.com/YAWAL/ERP-common-lib/models"
 )
 
+// EmployeeRepository is a methods set for datastores for employees related information
 type EmployeeRepository interface {
 	CreateEmployee(emp *models.Employee) error
 	UpdateEmployee(emp *models.Employee) error
-	DeleteEmployee(id int64) (bool, error)
+	DeleteEmployee(id string) (bool, error)
 	GetEmployees() (emps []*models.Employee, err error)
+	GetEmployeesByPosition(position string) (emps []*models.Employee, err error)
 
 	//GetEmployeeById(id int64) *Employee
 	//GetEmployeeByName(name string) []*Employee
 	//GetEmployeeByLastName(lastName string) []*Employee
 	//GetEmployeeByNameAndLastName(name, lastName string) []*Employee
 	//GetEmployeeByIdentificationNum(id int64) *Employee
-	//GetEmployeeByPosition(position string) []*Employee
 	//GetEmployeeByJoinDate(joinDate time.Time) []*Employee
 	//GetEmployeeByQuitDate(quitDate time.Time) []*Employee
 }

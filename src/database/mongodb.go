e769ab9c6b7638dc834ba77faa5813adefa41af0
YAWAL <yavorskyyval@gmail.com>
2019-11-08 01:07:49 +0200

    updated function for creating MongoDB client

--- src/database/mongodb.go
+++ src/database/mongodb.go
@@ -9,16 +9,17 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Mongo creates client to MongoDB with host and port provided in database config file part
+const mongoDBstring = "mongodb://%v:%v"
+
+// Mongo creates client to MongoDB with host and port provided in database config file's part
 func Mongo(conf MongoConfig) (mongoCl *mongo.Client, err error) {
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI(conf.Host, conf.Port)))
 	if err != nil {
 		return nil, err
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	if err != nil {
-		return nil, err
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	if err = client.Connect(ctx); err != nil {
 		return nil, err
 	}
@@ -26,5 +27,5 @@ func Mongo(conf MongoConfig) (mongoCl *mongo.Client, err error) {
 }
 
 func mongoURI(host, port string) string {
-	return fmt.Sprintf("mongodb://%v:%v", host, port)
+	return fmt.Sprintf(mongoDBstring, host, port)
 }

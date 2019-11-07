674fa4cc68a181ef761f5e96fd51ea343213d6d5
YAWAL <yavorskyyval@gmail.com>
2019-11-08 01:10:43 +0200

    implements handler functions for retreiving, deleting of employees

--- src/handlers/usecases.go
+++ src/handlers/usecases.go
@@ -9,6 +9,13 @@ import (
 	"github.com/YAWAL/ERP-common-lib/models"
 	"github.com/YAWAL/HumanResourceMicroservice/src/database"
 	"github.com/YAWAL/HumanResourceMicroservice/src/logging"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	positionParameter = "position"
+	idParameter       = "id"
 )
 
 // temporary HR index page
@@ -25,12 +32,8 @@ func TempIndexPage(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, HRwebpage)
 }
 
-// ShowAllEmployees renders all employees from database
+// ShowAllEmployees retrieves all employees from MongoDB
 // GET/ employees
-//func ShowAllEmployees(w http.ResponseWriter, r *http.Request) {
-//
-//}
-
 func ShowAllEmployees(er database.EmployeeRepository) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		employees, err := er.GetEmployees()
@@ -39,6 +42,7 @@ func ShowAllEmployees(er database.EmployeeRepository) func(http.ResponseWriter, 
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		logging.Log.Infof("ShowAllEmployees: %d employees have been retrieved", len(employees))
 		data, err := json.Marshal(employees)
 		if err != nil {
 			logging.Log.Errorf("ShowAllEmployees error: %s", err.Error())
@@ -56,10 +60,8 @@ func ShowAllEmployees(er database.EmployeeRepository) func(http.ResponseWriter, 
 	}
 }
 
-func ShowAllEmployeesByName(w http.ResponseWriter, r *http.Request) {
-
-}
-
+// CreateEmployee creates employee document in MongoDB's database "hr", collection "employees"
+// POST/ employees
 func CreateEmployee(er database.EmployeeRepository) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		b, err := ioutil.ReadAll(r.Body)
@@ -81,6 +83,82 @@ func CreateEmployee(er database.EmployeeRepository) func(http.ResponseWriter, *h
 			return
 		}
 		w.WriteHeader(http.StatusCreated)
+		return
+	}
+}
+
+// UpdateEmployee updates employee document in MongoDB's database "hr", collection "employees"
+// PUT/ employees
+func UpdateEmployee(er database.EmployeeRepository) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			logging.Log.Errorf("UpdateEmployee error: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+		var employee models.Employee
+		if err = json.Unmarshal(b, &employee); err != nil {
+			logging.Log.Errorf("UpdateEmployee error: %s", err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if err = er.UpdateEmployee(&employee); err != nil {
+			logging.Log.Errorf("UpdateEmployee error: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+}
+
+// DeleteEmployee delets employee document from MongoDB's database "hr", collection "employees"
+// DELETE/ employees/{id}
+func DeleteEmployee(er database.EmployeeRepository) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		deleted, err := er.DeleteEmployee(mux.Vars(r)[idParameter])
+		if err != nil {
+			logging.Log.Errorf("DeleteEmployee error: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if deleted {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+}
+
+// ShowAllEmployeesByPosition retrieves all employees from MongoDB
+// GET/ employees/{position}
+func ShowAllEmployeesByPosition(er database.EmployeeRepository) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		position := mux.Vars(r)[positionParameter]
+		employees, err := er.GetEmployeesByPosition(position)
+		if err != nil {
+			logging.Log.Errorf("ShowAllEmployeesByPosition error: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		logging.Log.Infof("ShowAllEmployeesByPosition: %d employees with position %s have been retrieved", len(employees), position)
+		data, err := json.Marshal(employees)
+		if err != nil {
+			logging.Log.Errorf("ShowAllEmployeesByPosition error: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("content-type", "application/json")
+		_, err = w.Write(data)
+		if err != nil {
+			logging.Log.Errorf("ShowAllEmployeesByPosition error: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		return
 	}
 }

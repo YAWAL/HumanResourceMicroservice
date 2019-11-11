5fdc6a26d4bfc0e71424deb2c378c4852d31170a
YAWAL <yavorskyyval@gmail.com>
2019-11-11 23:52:25 +0200

    removed database package

--- src/database/config.go
+++
@@ -1,7 +0,0 @@
-package database
-
-// MongoConfig is a structure which conteins details for creating MongoDB client
-type MongoConfig struct {
-	Host string `json:"host"`
-	Port string `json:"port"`
-}

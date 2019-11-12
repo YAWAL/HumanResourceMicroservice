a8be6cd2e1b642cb135b4bae780dd3dd17c86d62
YAWAL <yavorskyyval@gmail.com>
2019-11-12 22:42:00 +0200

    updated URL's paths

--- src/router/routes.go
+++ src/router/routes.go
@@ -17,7 +17,7 @@ func InitRouter(er repository.EmployeeRepository) (r *mux.Router) {
 	api.HandleFunc("/employees", handlers.ShowAllEmployees(er)).Methods(http.MethodGet)
 	api.HandleFunc("/employees", handlers.CreateEmployee(er)).Methods(http.MethodPost)
 	api.HandleFunc("/employees", handlers.UpdateEmployee(er)).Methods(http.MethodPut)
-	api.HandleFunc("/employees/{id:[0-9a-zA-Z]+}", handlers.DeleteEmployee(er)).Methods(http.MethodDelete)
-	api.HandleFunc("/employees/{position:[0-9a-zA-Z]+}", handlers.ShowAllEmployeesByPosition(er)).Methods(http.MethodGet)
+	api.HandleFunc("/employees/id/{id:[0-9a-zA-Z]+}", handlers.DeleteEmployee(er)).Methods(http.MethodDelete)
+	api.HandleFunc("/employees/position/{position:[a-zA-Z]+}", handlers.ShowAllEmployeesByPosition(er)).Methods(http.MethodGet)
 	return r
 }
